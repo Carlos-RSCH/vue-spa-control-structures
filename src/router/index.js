939412dbd651ce0f9e82a1d8e086aa8e0bfff855
Carlos-RSCH <l23270689@tuxtla.tecnm.mx>
2025-10-07 00:37:06 -0600

    Corrijo nombre de vista VIfView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import VIfView from '../views/VIfView.vue'
 import VShowView from '../views/VShowView.vue'
 import VForView from '../views/VForView.vue'
@@ -13,6 +13,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
